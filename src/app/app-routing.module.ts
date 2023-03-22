54e260466e7cbe7a82f39b3142fe4bf5636a7acc
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2023-03-22 19:59:36 -0400

    Week9 - EndCode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './content/home/home.component';
+import { ProjectsComponent } from './content/projects/projects.component';
+import { ServicesComponent } from './content/services/services.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { AboutComponent } from './content/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full' },
+  {path: 'home', component: HomeComponent },
+  {path: 'projects', component: ProjectsComponent },
+  {path: 'services', component: ServicesComponent },
+  {path: 'contact', component: ContactComponent },
+  {path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './content/home/home.component';
+import { ProjectsComponent } from './content/projects/projects.component';
+import { AboutComponent } from './content/about/about.component';
+import { ServicesComponent } from './content/services/services.component';
+import { ContactComponent } from './content/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
