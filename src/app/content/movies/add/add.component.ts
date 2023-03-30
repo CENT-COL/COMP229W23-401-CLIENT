4110cf6e1a4d50175f1004bf8eadf7dbac8e246b
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2023-03-29 20:32:01 -0400

    Add and Edit Components Completed

--- src/app/content/movies/add/add.component.ts
+++ src/app/content/movies/add/add.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'movie-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class MovieAddComponent {
+export class MovieAddComponent implements OnInit {
 
+  form: any = {
+    name: null,
+    year: null,
+    director: null,
+    genre: null,
+    runtime: null
+  }
+
+  isSuccessfull = true;
+  errorMessage = "";
+
+  constructor(private movieService: MoviesService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.movieService.addMovie(this.form)
+      .subscribe({
+        next: data => {
+          console.log(data);
+          this.isSuccessfull = true;
+          this.router.navigate(['/movie/list']);
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      })
+  }
 }
--- src/app/content/movies/edit/edit.component.ts
+++ src/app/content/movies/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'movie-edit',
@@ -7,4 +9,61 @@ import { Component } from '@angular/core';
 })
 export class MovieEditComponent {
 
+  form: any = {
+    _id: null,
+    name: null,
+    year: null,
+    director: null,
+    genre: null,
+    runtime: null
+  }
+
+  isSuccessfull = true;
+  errorMessage = "";
+
+  constructor(private movieService: MoviesService, 
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe({
+        next: params => {
+          this.form._id = params['id']; // :id 
+
+          this.movieService.getMovie(this.form._id)
+            .subscribe({
+              next: data => {
+                this.form = data.movie;
+              },
+              error: err => {
+                this.errorMessage = err.error.message;
+                this.isSuccessfull = false;
+              }
+            })
+
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      })
+
+  }
+
+  onSubmit(){
+    this.movieService.editMovie(this.form)
+      .subscribe({
+        next: data => {
+          console.log(data);
+          this.isSuccessfull = true;
+          this.router.navigate(['/movie/list']);
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      })
+  }
 }
